c82994ecb0e9de77370364694dd3b2f99931ae10
jeff <phijdo@gmail.com>
2021-12-15 16:13:22 -0500

    updated sign up, sign in, settings, and billing pages

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,11 +1,134 @@
-import React from 'react'
+import { useNavigate} from 'react-router-dom'
+import { useAuth } from '../AuthContext'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { useState } from 'react';
+import { Alert } from '@mui/material';
 
 const GetStarted = () => {
-  return (
-    <div>
-      <h1>GetStarted</h1>
-    </div>
-  )
+  const history = useNavigate();
+  const { signup, signupError } = useAuth();
+  const [ passwordMatchError, setPasswordMatchError ] = useState(null)
+
+  const handleSubmit = async (event) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      
+      const email = data.get('email');
+      const password = data.get('password');
+      const confirmedPassword = data.get('confirmPassword')
+      console.log({
+          email: email,
+          password: password,
+          confirmedpassword: confirmedPassword
+      });
+      if(password !== confirmedPassword) {
+          setPasswordMatchError('Passwords do not match!')
+          return;
+      } else {
+          setPasswordMatchError(null)
+      }
+
+      try { 
+
+          await signup(email, password); //sign up
+          if(signupError !== null) {
+              console.log("ERROR")
+              return
+          }
+          else {
+              history("/")
+              console.log("successfully signed up and switched to user page")
+          }
+          
+      } catch(err) {
+          console.log("ERROR: " + err);
+      }
+  };
+
+    
+    return (
+      <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+          sx={{
+              marginTop: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+          }}
+          >
+              { signupError && <Alert variant="filled" severity="error">{signupError}</Alert> }
+              { passwordMatchError && <Alert variant="filled" severity="error">{passwordMatchError}</Alert> }
+              <Avatar sx={{ m: 1, bgcolor: 'secondary' }}>
+                  <AccountCircleIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                  Sign up
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                  <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  />
+
+                  <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  />
+
+                  <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="current-password"
+                  />
+
+                  <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  >
+                  Sign Up
+                  </Button>
+
+                  <Grid item>
+                      <Link href="/login" variant="body2">
+                      {"Have an account? Sign In"}
+                      </Link>
+                  </Grid>
+
+              </Box>
+          </Box>
+      </Container>
+  );
+
 }
 
 export default GetStarted
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,61 +1,112 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../AuthContext'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import {makeStyles} from '@material-ui/core/styles';
-import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LoginIcon from '@mui/icons-material/Login';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { Alert } from '@mui/material';
 
 const Login = () => {
-  const navigate = useNavigate();
-  const useStyles = makeStyles({
-    text_field: {
-      border: 0,
-      borderRadius: 15,
-      padding: '5px 30px',
-      marginBottom: '10px',
-    },
-    btn: {
-      padding: '0 30px',
-      borderRadius: 15,
-    },
-  }); 
+  const history = useNavigate();
+    const { signin, signinError } = useAuth();
 
-  const classes = useStyles();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        
+        const email = data.get('email');
+        const password = data.get('password');
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    navigate('/');
-  };
+        console.log({
+            email: email,
+            password: password,
+        });
 
-  return (
-    <div>
-      <Typography variant='h2'>
-        Login
-     </Typography>
-      <form onSubmit={onSubmit}>
-        <div className={classes.text_field}>
-        <TextField
-            required
-            type="text"
-            id="outlined-required"
-            label="Enter Username"
-        />
-        </div>
-        <div className={classes.text_field}>
-          <TextField
-            required
-            type="password"
-            id="outlined-required"
-            label="Enter Password"
-        />
-        </div>
+        try { 
+            await signin(email, password); //sign in
+            if(signinError !== null) {
+                console.log("ERROR")
+                return
+            }
+            else {
+                history("/")
+                console.log("successfully signed in and switched to user page")
+            }
+        } catch(err) {
+            console.log("ERROR: " + err);
+        }
+    };
 
-        <div className={classes.btn}>
-          <Button variant="contained" type="submit">login</Button>
-        </div>
-      </form>
-    </div>
-  )
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            
+            <Box
+            sx={{
+                marginTop: 5,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+            >
+                {signinError && <Alert variant="filled" severity="error">{signinError}</Alert>} 
+                <Avatar sx={{ m: 1, bgcolor: 'secondary' }}>
+                    <LoginIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    />
+                    <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    />
+                    
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    >
+                    Login
+                    </Button>
+                    
+                    <Grid item>
+                        <Link href="/get_started" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                        </Link>
+                    </Grid>
+                    
+
+                </Box>
+            </Box>
+        </Container>
+    );
+
+
 }
 
 export default Login;
